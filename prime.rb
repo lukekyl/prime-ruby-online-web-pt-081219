9dac471448435ae30ecc44eca0624f7c4d119b04
Kyle Luke <th.oldschool@gmail.com>
2019-08-21 22:11:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -16,14 +16,3 @@ def prime?(integer)
  
 end
 
-def prime?(n)
-    if n <= 1
-      return false
-    else 
-        if (2...n).any? { |i| n % i  == 0}
-            return false
-        else
-            return true
-        end 
-    end
-end
