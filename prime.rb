40cc2c2ddb5b7e0f60f93273bb49b1031e27716c
Kyle Luke <th.oldschool@gmail.com>
2019-08-21 22:17:12 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+
+def prime?(integer)
+  if integer < 0 
+    new_integer = integer * -1
+  else
+    new_integer = integer
+  end 
+  
+  if new_integer == 1 || new_integer == 0
+      return false
+  elsif (2...new_integer).any? {|i| new_integer % i == 0}
+      return false
+  else 
+    return true
+  end
+end
+
