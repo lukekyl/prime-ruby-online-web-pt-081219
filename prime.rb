3ddce400d16f0b4284478fe89bf948ca32bc350e
Kyle Luke <th.oldschool@gmail.com>
2019-08-21 21:46:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,13 @@
 
 def prime?(integer)
-  (2..integer).each do |i|
-    if integer % i == 0
+  if integer < 0 
+    new_integer = integer * -1
+  else
+    new_integer = integer
+  end 
+  
+  (2..new_integer).each do |i|
+    if new_integer % i == 0
       return false
     else
       return true
